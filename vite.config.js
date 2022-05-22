843e45c6abec340a7b47a29fca64d65ac43c5a2c
Sean Boerhout <boeseany@gmail.com>
2022-05-22 12:45:11 -0700

    vite updages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 
+const { resolve } = require('path')
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
@@ -8,4 +10,21 @@ export default defineConfig({
         ? 443
         : undefined,
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        game: resolve(__dirname, 'game.html'),
+        gameover: resolve(__dirname, 'gameover.html'),
+        answer: resolve(__dirname, 'answer.html'),
+      }
+    }
+  },
+
 });
+
+
+
+
+
+
